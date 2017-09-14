734a9ff90a3361e04b19b5740e01f12313ae6d91
komala <komaladeepthi@gmail.com>
2017-09-13 20:46:29 -0700

    added javascript to bind values using angularJs

--- assets/calculator.js
+++ assets/calculator.js
@@ -1,5 +1,24 @@
 /* JavaScript Document */
 
-/* Script goes here */
+(function(){
+
+	var app = angular.module('mycalculator',[]);
+	app.controller('CalculatorController',['$scope',function($scope){
+
+			$scope.lumen_options = [375,600,900,1125,1600];
+			$scope.current_lumens = 600;
+			$scope.current_cost = 12;
+			$scope.current_hours = 3;
+			$scope.total_days = 365;
+
+
+	}]);
+
+
+
+
+
+
+})();
 
 
